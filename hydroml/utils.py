258dec1a42dcd4c5600f6a131ab57e6294b528b7
arbennett <bennett.andr@gmail.com>
2022-06-30 20:11:29 -0400

    Add helper for resuming from checkpoint

--- hydroml/utils.py
+++ hydroml/utils.py
@@ -1,5 +1,7 @@
 import torch
+import mlflow
 import numpy as np
+import pandas as pd
 from tqdm.autonotebook import tqdm
 from pytorch_lightning import Callback
 from pytorch_lightning.callbacks.progress import ProgressBar
@@ -16,6 +18,9 @@ class MetricsCallback(Callback):
     def __init__(self):
         super().__init__()
         self.metrics = {}
+
+    #def on_train_start(self, trainer, pl_module):
+    #    print(trainer.optimizers[0].state_dict())
 
     def on_validation_epoch_end(self, trainer, pl_module):
         for k, v in trainer.logged_metrics.items():
@@ -38,3 +43,39 @@ class LitProgressBar(ProgressBar):
     def init_validation_tqdm(self):
         bar = tqdm(disable=True,)
         return bar
+
+
+def find_best_checkpoint(
+    log_dir,
+    experiment_name,
+    uri_scheme='file:',
+    uri_authority='',
+):
+    tracking_uri = f'{uri_scheme}{uri_authority}{log_dir}'
+    mlflow.set_tracking_uri(tracking_uri)
+    client = mlflow.tracking.MlflowClient()
+
+    experiment = client.get_experiment_by_name(experiment_name)
+    experiment_id = experiment.experiment_id
+    runs = client.list_run_infos(experiment_id)
+    # Assumes first run is current and second is the most recently completed
+    test_run = runs[1]
+    run_id = test_run.run_id
+    run_path = f'{log_dir}/{experiment_id}/{run_id}'
+    run_dict = mlflow.get_run(run_id).to_dictionary()
+    checkpoint_dir = run_dict['data']['params']['checkpoint_dir']
+    tracking_metric = 'train_loss'
+    metric_df = pd.read_csv(f'{run_path}/metrics/{tracking_metric}',
+                            delim_whitespace=True,
+                            names=['time', tracking_metric, 'step'],
+                            index_col=2)
+    epoch_df = pd.read_csv(f'{run_path}/metrics/epoch',
+                            delim_whitespace=True,
+                            names=['time', 'epoch', 'step'],
+                            index_col=2)
+
+    best_step = metric_df[tracking_metric].idxmin()+1
+    current_epoch = int(epoch_df.loc[best_step-1]['epoch'])
+    checkpoint_file = f'epoch={current_epoch}-step={best_step}.ckpt'
+    best_checkpoint = f'{checkpoint_dir}/{checkpoint_file}'
+    return best_checkpoint
