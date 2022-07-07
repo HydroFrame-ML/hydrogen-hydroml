353332e860bd1e43d02d2462061c088a396b41d6
wh3248 <wh3248@princeton.edu>
2022-07-07 14:35:07 -0400

    resolve merge conflict

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 
 setup(
     name='hydroml',
-    packages=['hydroml'],
     python_requires='>3.6',
     packages=['hydroml', 'hydroml.scalers'],
     install_requires=[
