690daea76859bdd0c7f97e69bb50ef4cb9fd3f1b
aktriplett <aktriplett@email.arizona.edu>
2022-11-21 15:28:54 -0700

    adding new loss function DWSE

--- hydroml/loss.py
+++ hydroml/loss.py
@@ -7,3 +7,20 @@ def MWSE(y_hat, y_true, weights, dim=([0], [1]), norm_weights=True):
     square_err = (y_true - y_hat) ** 2
     weighted_se = torch.tensordot(weights, square_err, dims=dim) / len(weights)
     return torch.mean(weighted_se)
+
+
+def DWSE(y_hat, y_true, lmda, loss_func):
+    std_loss = loss_func(y_hat, y_true)
+    
+    dx_true = y_true[...,0:-1]-y_true[...,1:]
+    dy_true = y_true[...,0:-1,:] - y_true[...,1:,:]
+        
+    dx_pred = y_hat[...,0:-1]-y_hat[...,1:]
+    dy_pred = y_hat[...,0:-1,:] - y_hat[...,1:,:]
+
+    dx_loss = torch.mean(lmda*(abs(dx_pred-dx_true))) #This result is a grid of dx diff
+    dy_loss = torch.mean(lmda*(abs(dy_pred-dy_true))) #this result is a grid of dy diff
+
+    deriv_loss = std_loss + dx_loss + dy_loss
+
+    return deriv_loss
